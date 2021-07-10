bbbd1e158adfefd8159c875f01c5bca6ec4ff0fc
Afrie Irham <itsafrie@gmail.com>
2021-07-10 22:08:15 +0800

    Update favicon and make it responsive on mobile

--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,91 @@
 import Head from 'next/head'
-import { Button, Flex, Heading, Image, Text, Link, Box, IconButton, HStack } from '@chakra-ui/react'
-import { ArrowRight, GitHub, Linkedin, Mail, Twitter, Youtube } from 'react-feather'
+import {
+  Flex,
+  Heading,
+  Image,
+  Text,
+  Link,
+  Box,
+  Stack,
+  useBreakpointValue,
+  Icon,
+} from '@chakra-ui/react'
+import { GitHub, Linkedin, Mail } from 'react-feather'
 
-function Icon({ name: IconName, href }) {
+function LinkIcon({ name, href }) {
   return (
-    <IconButton
-      as={Link}
-      icon={<IconName />}
-      variant='ghost'
-      borderRadius='999px'
-      color='gray.400'
-      href={href}
-      isExternal
-    />
+    <Link href={href} isExternal>
+      <Icon as={name} color='gray.400' _hover={{ color: 'red.300' }} boxSize='1.5em' focusable />
+    </Link>
+  )
+}
+
+function LinkText({ children, href }) {
+  return (
+    <Link textDecoration='underline' _hover={{ color: 'red.400' }} href={href} isExternal>
+      {children}
+    </Link>
   )
 }
 
 export default function Home() {
+  const heading = useBreakpointValue({ base: 'xl', sm: 'xl' })
   return (
-    <div>
+    <>
       <Head>
         <title>Afrie Irham</title>
         <link rel='icon' href='/favicon.ico' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
         <link
           href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
           rel='stylesheet'
         ></link>
       </Head>
 
-      <Flex minHeight='100vh' width='100%' justifyContent='center' alignItems='center'>
-        <Box mr={16}>
-          <Image src='/portrait.png' height='200' />
+      <Flex
+        minHeight='100vh'
+        justifyContent={{ base: 'start', sm: 'center' }}
+        alignItems={{ base: 'start', sm: 'center' }}
+        direction={{ base: 'column', sm: 'row' }}
+        paddingX={{ base: 4, sm: 0 }}
+      >
+        <Box mr={{ base: 0, sm: 16 }} mb={{ base: 8, sm: 0 }} pt={{ base: 16, sm: 0 }}>
+          <Image src='/portrait.png' height={{ base: '125', sm: '200' }} />
         </Box>
-        <Box maxWidth='500px'>
-          <Heading size='2xl' fontWeight='extrabold' mb={6}>
-            Hi, I'm Afrie Irham 🇲🇾
+        <Flex maxWidth='550px' direction='column'>
+          <Text color='gray.500' textTransform='uppercase' fontSize={{ base: 'xs', sm: 'sm' }}>
+            Hi, I'm Afrie Irham 👋
+          </Text>
+          <Heading
+            size={heading}
+            fontWeight='extrabold'
+            mb={{ base: 4, sm: 6 }}
+            letterSpacing='tight'
+          >
+            Software developer who loves to create and build stuff.
           </Heading>
-          <Text fontSize='2xl' mb={4}>
-            <Text
-              as='span'
-              fontWeight='bold'
-              background='radial-gradient( circle 870px at 5.8% 56%,  rgba(255,3,3,1) 0%, rgba(249,232,27,1) 89.2% )'
-              style={{ WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}
-            >
-              Software Developer
-            </Text>{' '}
-            who loves to create and build stuff.
+          <Text mb={8} color='gray.600'>
+            I'm based in Malaysia, currently at{' '}
+            <LinkText href='https://snappymob.com'>Snappymob</LinkText> as a software developer and
+            I love React. Built{' '}
+            <LinkText href='https://bukugraduan.afrieirham.com'>BukuGraduan</LinkText> and{' '}
+            <LinkText href='https://bayarsini.link'>BayarSini</LinkText> with Next.js. Ohh –
+            sometimes I <LinkText href='https://blog.afrieirham.com'>write</LinkText> and make{' '}
+            <LinkText href='https://youtube.com/c/madebyafrie'>videos</LinkText> but I do{' '}
+            <LinkText href='https://twitter.com/afrieirham_'>tweet</LinkText> a lot.
           </Text>
-          <Text mb={8} fontSize='sm' color='gray.700'>
-            Currently at{' '}
-            <Link href='https://snappymob.com' isExternal>
-              Snappymob
-            </Link>{' '}
-            👨🏻‍💻
+
+          <Text mb={2} color='gray.600'>
+            Say hi to me.
           </Text>
-          <HStack>
-            <Button
-              color='white'
-              backgroundColor='black'
-              borderRadius='999px'
-              padding='4'
-              fontWeight='bold'
-              rightIcon={<ArrowRight size='15' />}
-              boxShadow='rgb(0, 0, 0) 0px 10px 20px -10px'
-              _hover={{
-                backgroundColor: 'black',
-                boxShadow: 'rgb(0, 0, 0) 0px 5px 10px -5px',
-                textDecoration: 'none',
-              }}
-              _active={{ backgroundColor: 'black', boxShadow: 'rgb(0, 0, 0) 0px 3px 5px -3px' }}
-              mr='4'
-              as={Link}
-              href='https://blog.afrieirham.com'
-              isExternal
-            >
-              Read my blog
-            </Button>
-            <Icon name={Mail} href='mailto:afrieirham@outlook.com' />
-            <Icon name={Linkedin} href='https://linkedin.com/in/afrieirham' />
-            <Icon name={Twitter} href='https://twitter.com/afrieirham_' />
-            <Icon name={Youtube} href='https://youtube.com/c/madebyafrie' />
-            <Icon name={GitHub} href='https://github.com/afrieirham' />
-          </HStack>
-        </Box>
+          <Stack direction='row' spacing='4'>
+            <LinkIcon name={Mail} href='mailto:afrieirham@outlook.com' />
+            <LinkIcon name={Linkedin} href='https://linkedin.com/in/afrieirham' />
+            <LinkIcon name={GitHub} href='https://github.com/afrieirham' />
+          </Stack>
+        </Flex>
       </Flex>
-    </div>
+    </>
   )
 }
